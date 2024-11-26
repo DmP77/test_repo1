ec7a97740ccf3659308891a141fa6d6925ec4eba
Dmp <ussr2552@gmail.com>
2024-11-26 11:47:09 +0100

    changed 2 files

--- monitoring.sh
+++ monitoring.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Версия операционной системы
-# edtrwerrtwertwerwerew
-# fserfwerwerwe
+
+# fserfwerwerwe12345
 os_version=$(cat /etc/os-release | grep "PRETTY_NAME" | cut -d '"' -f 2)
 
 # Дата и время
